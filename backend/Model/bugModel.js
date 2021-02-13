d1270275127baa65ab999538afb3da0f1bc9370e
JenniferR326 <jennarafael@gmail.com>
2021-02-13 16:40:43 -0500

    bug model added

--- backend/Model/bugModel.js
+++ backend/Model/bugModel.js
@@ -1,1 +1,30 @@
 //bug model info
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db/database')
+
+const Bug = db.define('bug', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  details: {
+    type: Sequelize.STRING
+  },
+  steps: {
+    type: Sequelize.STRING
+  },
+  priority: {
+    type: Sequelize.INTEGER
+  },
+  creator: {
+    type: Sequelize.STRING
+  },
+  time: {
+    type: Sequelize.TIME
+  }
+})
+
+
+module.exports = Bug
